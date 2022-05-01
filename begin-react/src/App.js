b7d4161562dc6a54f261cf4416771cc46d691e35
홍은표 <44293278+potato179@users.noreply.github.com>
2022-05-01 21:00:20 +0900

    gay-joy-go

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,12 +1,25 @@
 import React from "react";
 import Hello from "./hello";
+import "./App.css";
 
 function App() {
+    const name = "꺼져!";
+    const style = {
+        backgroundColor: "red",
+        color: "blue",
+        fontSize: 24,
+        padding: "1rem"
+    };
     return (
         <div>
+            {/* 주현준은 게이다 */}
+            <p>안녕하세요.</p>
             <Hello></Hello>
             <Hello></Hello>
             <Hello></Hello>
+            <input></input>
+            <p style = {style}>{name}</p>
+            <div className = "gray-box"></div>
         </div>
     );
 }
